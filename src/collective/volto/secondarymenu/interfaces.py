9f607e8d42290cc9ee643537f19dcfe98abe4336
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-01-28 11:01:39 +0100

    fix changelog

---
+++ src/collective/volto/secondarymenu/interfaces.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from collective.volto.secondarymenu import _
+from plone.restapi.controlpanels.interfaces import IControlpanel
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.schema import SourceText
+import json
+
+
+class ISecondaryMenu(IControlpanel):
+    secondary_menu_configuration = SourceText(
+        title=_(
+            "secondary_menu_configuration_label", default="Menu configuration"
+        ),
+        description="",
+        required=True,
+        default=json.dumps([{"rootPath": "/", "items": []}]),
+    )
+
+
+class ICollectiveVoltoSecondaryMenuLayer(IDefaultBrowserLayer):
+    """Marker interface that defines a browser layer."""
